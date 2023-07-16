805f2de076d49240e28350a3fcc1c00f130e61b6
treaty321 <2117ab@gmail.com>
2023-07-16 22:16:32 +0900

    add allowed_hosts

--- awsProject/settings.py
+++ awsProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-498&hk09bw-cll30cf08tj!gwvyvq!51or*pqertojfk+^d==l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".ap-northeast-2.compute.amazonaws.com"]
 
 
 # Application definition
